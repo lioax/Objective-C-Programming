2b8f8936a59dee931af2cd8081ab9b9bfda49fad
LioAx <lioaxos@gmail.com>
2015-11-11 16:26:12 -0700

    PrintF() example

---
+++ Squarer/Squarer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Squarer
+//
+//  Created by Lucas Alves on 11/9/15.
+//  Copyright © 2015 Lioax Productions. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ Triangle/Triangle/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Triangle
+//
+//  Created by Lucas Alves on 11/1/15.
+//  Copyright © 2015 Lioax Productions. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
